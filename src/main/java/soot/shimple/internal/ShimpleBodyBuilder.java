3ba50928090480fe02e2c3e32997b55670eeda23
Anastasios Antoniadis <anantoni@di.uoa.gr>
2018-08-02 13:24:28 +0300

    Change Shimple local variable name mangling

--- src/main/java/soot/shimple/internal/ShimpleBodyBuilder.java
+++ src/main/java/soot/shimple/internal/ShimpleBodyBuilder.java
@@ -1,84 +1,62 @@
-package soot.shimple.internal;
-
-/*-
- * #%L
- * Soot - a J*va Optimization Framework
- * %%
+/* Soot - a J*va Optimization Framework
  * Copyright (C) 2003 Navindra Umanee <navindra@cs.mcgill.ca>
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 2.1 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-2.1.html>.
- * #L%
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Stack;
+package soot.shimple.internal;
 
-import soot.Local;
-import soot.Unit;
-import soot.Value;
-import soot.ValueBox;
-import soot.jimple.DefinitionStmt;
-import soot.jimple.internal.JimpleLocal;
-import soot.jimple.toolkits.base.Aggregator;
-import soot.jimple.toolkits.scalar.DeadAssignmentEliminator;
-import soot.jimple.toolkits.scalar.LocalNameStandardizer;
-import soot.jimple.toolkits.scalar.NopEliminator;
-import soot.jimple.toolkits.scalar.UnconditionalBranchFolder;
-import soot.jimple.toolkits.scalar.UnreachableCodeEliminator;
-import soot.options.ShimpleOptions;
-import soot.shimple.DefaultShimpleFactory;
-import soot.shimple.PhiExpr;
-import soot.shimple.Shimple;
-import soot.shimple.ShimpleBody;
-import soot.shimple.ShimpleFactory;
-import soot.toolkits.graph.Block;
-import soot.toolkits.graph.BlockGraph;
-import soot.toolkits.graph.DominatorNode;
-import soot.toolkits.graph.DominatorTree;
-import soot.toolkits.scalar.UnusedLocalEliminator;
+import soot.*;
+import java.util.*;
+import soot.shimple.*;
+import soot.options.*;
+import soot.jimple.*;
+import soot.jimple.internal.*;
+import soot.jimple.toolkits.base.*;
+import soot.jimple.toolkits.scalar.*;
+import soot.toolkits.graph.*;
+import soot.toolkits.scalar.*;
 
 /**
- * This class does the real high-level work. It takes a Jimple body or Jimple/Shimple hybrid body and produces pure Shimple.
+ * This class does the real high-level work.  It takes a Jimple body
+ * or Jimple/Shimple hybrid body and produces pure Shimple.
  *
- * <p>
- * The work is done in two main steps:
+ * <p> The work is done in two main steps:
  *
  * <ol>
- * <li>Trivial Phi nodes are added.
- * <li>A renaming algorithm is executed.
+ * <li> Trivial Phi nodes are added.
+ * <li> A renaming algorithm is executed.
  * </ol>
  *
- * <p>
- * This class can also translate out of Shimple by producing an equivalent Jimple body with all Phi nodes removed.
+ * <p> This class can also translate out of Shimple by producing an
+ * equivalent Jimple body with all Phi nodes removed.
  *
- * <p>
- * Note that this is an internal class, understanding it should not be necessary from a user point-of-view and relying on it
+ * <p> Note that this is an internal class, understanding it should
+ * not be necessary from a user point-of-view and relying on it
  * directly is not recommended.
  *
  * @author Navindra Umanee
  * @see soot.shimple.ShimpleBody
- * @see <a href="http://citeseer.nj.nec.com/cytron91efficiently.html">Efficiently Computing Static Single Assignment Form and
- *      the Control Dependence Graph</a>
+ * @see <a
+ * href="http://citeseer.nj.nec.com/cytron91efficiently.html">Efficiently
+ * Computing Static Single Assignment Form and the Control Dependence
+ * Graph</a>
  **/
-public class ShimpleBodyBuilder {
+public class ShimpleBodyBuilder
+{
   protected ShimpleBody body;
   protected ShimpleFactory sf;
   protected DominatorTree<Block> dt;
@@ -94,17 +72,13 @@ public class ShimpleBodyBuilder {
 
   ShimpleOptions options;
 
+  final String ssaSeparator = "_$$A_";
+
   /**
    * Transforms the provided body to pure SSA form.
    **/
-  public ShimpleBodyBuilder(ShimpleBody body) {
-    // Must remove nops prior to building the CFG because NopStmt appearing
-    // before the IdentityStmt in a trap handler that is itself protected
-    // by a trap cause Phi nodes to be inserted before the NopStmt and
-    // therefore before the IdentityStmt. This introduces a validation
-    // problem if the Phi nodes leave residual assignment statements after
-    // their removal.
-    NopEliminator.v().transform(body);
+  public ShimpleBodyBuilder(ShimpleBody body)
+  {
     this.body = body;
     sf = new DefaultShimpleFactory(body);
     sf.clearCache();
@@ -114,23 +88,26 @@ public class ShimpleBodyBuilder {
     makeUniqueLocalNames();
   }
 
-  public void update() {
+  public void update()
+  {
     cfg = sf.getBlockGraph();
     dt = sf.getDominatorTree();
     origLocals = new ArrayList<Local>(body.getLocals());
   }
 
-  public void transform() {
+  public void transform()
+  {
     phi.insertTrivialPhiNodes();
 
     boolean change = false;
-    if (options.extended()) {
+    if(options.extended()){
       change = pi.insertTrivialPiNodes();
 
-      while (change) {
-        if (phi.insertTrivialPhiNodes()) {
+      while(change){
+        if(phi.insertTrivialPhiNodes()){
           change = pi.insertTrivialPiNodes();
-        } else {
+        }
+        else{
           break;
         }
       }
@@ -141,18 +118,20 @@ public class ShimpleBodyBuilder {
     makeUniqueLocalNames();
   }
 
-  public void preElimOpt() {
-    // boolean optElim = options.node_elim_opt();
+  public void preElimOpt()
+  {
+    //boolean optElim = options.node_elim_opt();
 
     // *** FIXME: 89e9a0470601091906j26489960j65290849dbe0481f@mail.gmail.com
-    // if(optElim)
-    // DeadAssignmentEliminator.v().transform(body);
+    //if(optElim)
+    //DeadAssignmentEliminator.v().transform(body);
   }
 
-  public void postElimOpt() {
+  public void postElimOpt()
+  {
     boolean optElim = options.node_elim_opt();
 
-    if (optElim) {
+    if(optElim){
       DeadAssignmentEliminator.v().transform(body);
       UnreachableCodeEliminator.v().transform(body);
       UnconditionalBranchFolder.v().transform(body);
@@ -162,22 +141,24 @@ public class ShimpleBodyBuilder {
   }
 
   /**
-   * Remove Phi nodes from current body, high probablity this destroys SSA form.
+   * Remove Phi nodes from current body, high probablity this
+   * destroys SSA form.
    *
-   * <p>
-   * Dead code elimination + register aggregation are performed as recommended by Cytron. The Aggregator looks like it could
+   * <p> Dead code elimination + register aggregation are performed
+   * as recommended by Cytron.  The Aggregator looks like it could
    * use some improvements.
    *
    * @see soot.options.ShimpleOptions
    **/
-  public void eliminatePhiNodes() {
-    if (phi.doEliminatePhiNodes()) {
+  public void eliminatePhiNodes()
+  {
+    if(phi.doEliminatePhiNodes())
       makeUniqueLocalNames();
-    }
 
   }
 
-  public void eliminatePiNodes() {
+  public void eliminatePiNodes()
+  {
     boolean optElim = options.node_elim_opt();
     pi.eliminatePiNodes(optElim);
   }
@@ -190,16 +171,18 @@ public class ShimpleBodyBuilder {
   /**
    * Maps renamed Locals to original Locals.
    **/
-  protected Map<Local, Local> newLocalsToOldLocal;
+  protected Map<Local,Local> newLocalsToOldLocal;
 
   protected int[] assignmentCounters;
   protected Stack<Integer>[] namingStacks;
 
   /**
-   * Variable Renaming Algorithm from Cytron et al 91, P26-8, implemented in various bits and pieces by the next functions.
+   * Variable Renaming Algorithm from Cytron et al 91, P26-8,
+   * implemented in various bits and pieces by the next functions.
    * Must be called after trivial nodes have been added.
    **/
-  public void renameLocals() {
+  public void renameLocals()
+  {
     update();
     newLocals = new HashMap<String, Local>();
     newLocalsToOldLocal = new HashMap<Local, Local>();
@@ -207,19 +190,16 @@ public class ShimpleBodyBuilder {
     assignmentCounters = new int[origLocals.size()];
     namingStacks = new Stack[origLocals.size()];
 
-    for (int i = 0; i < namingStacks.length; i++) {
+    for(int i = 0; i < namingStacks.length; i++)
       namingStacks[i] = new Stack<Integer>();
-    }
 
     List<Block> heads = cfg.getHeads();
 
-    if (heads.isEmpty()) {
+    if(heads.isEmpty())
       return;
-    }
 
-    if (heads.size() != 1) {
+    if(heads.size() != 1)
       throw new RuntimeException("Assertion failed:  Only one head expected.");
-    }
 
     Block entry = heads.get(0);
     renameLocalsSearch(entry);
@@ -228,7 +208,8 @@ public class ShimpleBodyBuilder {
   /**
    * Driven by renameLocals().
    **/
-  public void renameLocalsSearch(Block block) {
+  public void renameLocalsSearch(Block block)
+  {
     // accumulated in Step 1 to be re-processed in Step 4
     List<Local> lhsLocals = new ArrayList<Local>();
 
@@ -240,24 +221,21 @@ public class ShimpleBodyBuilder {
         {
           List<ValueBox> useBoxes = new ArrayList<ValueBox>();
 
-          if (!Shimple.isPhiNode(unit)) {
+          if(!Shimple.isPhiNode(unit))
             useBoxes.addAll(unit.getUseBoxes());
-          }
 
           for (ValueBox useBox : useBoxes) {
             Value use = useBox.getValue();
             int localIndex = indexOfLocal(use);
 
             // not one of our locals
-            if (localIndex == -1) {
+            if(localIndex == -1)
               continue;
-            }
 
             Local localUse = (Local) use;
 
-            if (namingStacks[localIndex].empty()) {
+            if(namingStacks[localIndex].empty())
               continue;
-            }
 
             Integer subscript = namingStacks[localIndex].peek();
 
@@ -268,18 +246,16 @@ public class ShimpleBodyBuilder {
 
         // Step 1 of 1
         {
-          if (!(unit instanceof DefinitionStmt)) {
+          if(!(unit instanceof DefinitionStmt))
             continue;
-          }
 
           DefinitionStmt defStmt = (DefinitionStmt) unit;
 
           Value lhsValue = defStmt.getLeftOp();
 
           // not something we're interested in
-          if (!origLocals.contains(lhsValue)) {
+          if(!origLocals.contains(lhsValue))
             continue;
-          }
 
           ValueBox lhsLocalBox = defStmt.getLeftOpBox();
           Local lhsLocal = (Local) lhsValue;
@@ -288,9 +264,8 @@ public class ShimpleBodyBuilder {
           lhsLocals.add(lhsLocal);
 
           int localIndex = indexOfLocal(lhsLocal);
-          if (localIndex == -1) {
+          if(localIndex == -1)
             throw new RuntimeException("Assertion failed.");
-          }
 
           Integer subscript = assignmentCounters[localIndex];
 
@@ -308,35 +283,30 @@ public class ShimpleBodyBuilder {
     {
       for (Block succ : cfg.getSuccsOf(block)) {
         // Ignore dummy blocks
-        if (block.getHead() == null && block.getTail() == null) {
+        if (block.getHead() == null && block.getTail() == null)
           continue;
-        }
 
         for (Unit unit : succ) {
           PhiExpr phiExpr = Shimple.getPhiExpr(unit);
 
-          if (phiExpr == null) {
+          if(phiExpr == null)
             continue;
-          }
 
           // simulate whichPred
           int argIndex = phiExpr.getArgIndex(block);
-          if (argIndex == -1) {
+          if(argIndex == -1)
             throw new RuntimeException("Assertion failed.");
-          }
 
           ValueBox phiArgBox = phiExpr.getArgBox(argIndex);
 
           Local phiArg = (Local) phiArgBox.getValue();
 
           int localIndex = indexOfLocal(phiArg);
-          if (localIndex == -1) {
+          if(localIndex == -1)
             throw new RuntimeException("Assertion failed.");
-          }
 
-          if (namingStacks[localIndex].empty()) {
+          if(namingStacks[localIndex].empty())
             continue;
-          }
 
           Integer subscript = namingStacks[localIndex].peek();
 
@@ -354,7 +324,7 @@ public class ShimpleBodyBuilder {
 
       Iterator<DominatorNode<Block>> childrenIt = dt.getChildrenOf(node).iterator();
 
-      while (childrenIt.hasNext()) {
+      while(childrenIt.hasNext()){
         DominatorNode<Block> childNode = childrenIt.next();
 
         renameLocalsSearch(childNode.getGode());
@@ -365,42 +335,41 @@ public class ShimpleBodyBuilder {
     {
       Iterator<Local> lhsLocalsIt = lhsLocals.iterator();
 
-      while (lhsLocalsIt.hasNext()) {
+      while(lhsLocalsIt.hasNext()){
         Local lhsLocal = lhsLocalsIt.next();
 
         int lhsLocalIndex = indexOfLocal(lhsLocal);
-        if (lhsLocalIndex == -1) {
+        if(lhsLocalIndex == -1)
           throw new RuntimeException("Assertion failed.");
-        }
 
         namingStacks[lhsLocalIndex].pop();
       }
     }
 
-    /* And we're done. The renaming process is complete. */
+    /* And we're done.  The renaming process is complete. */
   }
 
   /**
-   * Clever convenience function to fetch or create new Local's given a Local and the desired subscript.
+   * Clever convenience function to fetch or create new Local's
+   * given a Local and the desired subscript.
    **/
-  protected Local fetchNewLocal(Local local, Integer subscript) {
+  protected Local fetchNewLocal(Local local, Integer subscript)
+  {
     Local oldLocal = local;
 
-    if (!origLocals.contains(local)) {
+    if(!origLocals.contains(local))
       oldLocal = newLocalsToOldLocal.get(local);
-    }
 
-    if (subscript.intValue() == 0) {
+    if(subscript.intValue() == 0)
       return oldLocal;
-    }
 
     // If the name already exists, makeUniqueLocalNames() will
     // take care of it.
-    String name = oldLocal.getName() + "_" + subscript;
+    String name = oldLocal.getName() + ssaSeparator + subscript;
 
     Local newLocal = newLocals.get(name);
 
-    if (newLocal == null) {
+    if(newLocal == null){
       newLocal = new JimpleLocal(name, oldLocal.getType());
       newLocals.put(name, newLocal);
       newLocalsToOldLocal.put(newLocal, oldLocal);
@@ -413,13 +382,15 @@ public class ShimpleBodyBuilder {
   }
 
   /**
-   * Convenient function that maps new Locals to the originating Local, and finds the appropriate array index into the naming
+   * Convenient function that maps new Locals to the originating
+   * Local, and finds the appropriate array index into the naming
    * structures.
    **/
-  protected int indexOfLocal(Value local) {
+  protected int indexOfLocal(Value local)
+  {
     int localIndex = origLocals.indexOf(local);
 
-    if (localIndex == -1) {
+    if(localIndex == -1){
       // might be null
       Local oldLocal = newLocalsToOldLocal.get(local);
 
@@ -430,10 +401,12 @@ public class ShimpleBodyBuilder {
   }
 
   /**
-   * Make sure the locals in the given body all have unique String names. Renaming is done if necessary.
+   * Make sure the locals in the given body all have unique String
+   * names.  Renaming is done if necessary.
    **/
-  public void makeUniqueLocalNames() {
-    if (options.standard_local_names()) {
+  public void makeUniqueLocalNames()
+  {
+    if(options.standard_local_names()){
       LocalNameStandardizer.v().transform(body);
       return;
     }
@@ -441,30 +414,31 @@ public class ShimpleBodyBuilder {
     Set<String> localNames = new HashSet<String>();
     Iterator<Local> localsIt = body.getLocals().iterator();
 
-    while (localsIt.hasNext()) {
+    while(localsIt.hasNext()){
       Local local = localsIt.next();
       String localName = local.getName();
 
-      if (localNames.contains(localName)) {
+      if(localNames.contains(localName)){
         String uniqueName = makeUniqueLocalName(localName, localNames);
         local.setName(uniqueName);
         localNames.add(uniqueName);
-      } else {
-        localNames.add(localName);
       }
+      else
+        localNames.add(localName);
     }
   }
 
   /**
-   * Given a set of Strings, return a new name for dupName that is not currently in the set.
+   * Given a set of Strings, return a new name for dupName that is
+   * not currently in the set.
    **/
-  public String makeUniqueLocalName(String dupName, Set<String> localNames) {
+  public String makeUniqueLocalName(String dupName, Set<String> localNames)
+  {
     int counter = 1;
     String newName = dupName;
 
-    while (localNames.contains(newName)) {
-      newName = dupName + "_" + counter++;
-    }
+    while(localNames.contains(newName))
+      newName = dupName + ssaSeparator + counter++;
 
     return newName;
   }
